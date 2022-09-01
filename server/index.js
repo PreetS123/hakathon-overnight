b2fb3bdd739ea87593ce00624d914e783ba94b40
PreetS12301~ <preetbhardwaj517@gmail.com>
2022-09-01 06:49:00 +0530

    inital-setup

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-     
-
-     const express= require('express');
-     const cors= require('cors');
-     require('dotenv').config();
-
-     const app= express();
 
 
-     app.use(express.json());
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+
+app.use(express.json());
 
 
 
-     app.listen(process.env.PORT,()=>{
-        console.log(`Listening on port ${process.env.PORT}`);
-     })
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
