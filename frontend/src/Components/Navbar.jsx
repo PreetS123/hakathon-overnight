6037f8ef495823d3f39939cddf75d1892e297b2c
MindofSkk <helpmeskk@gmail.com>
2022-09-01 12:27:23 +0530

    navbar_done

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,101 @@
-import React from 'react'
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Image,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+// const NavLink = ({ children }) => (
+//   <Link
+//     px={2}
+//     py={1}
+//     rounded={'md'}
+//     _hover={{
+//       textDecoration: 'none',
+//       bg: useColorModeValue('gray.200', 'gray.700'),
+//     }}
+//     href={'#'}>
+//     {children}
+//   </Link>
+// );
 
 export const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box>
+            {/* logo */}
+            <Image
+              src={
+                "https://previews.123rf.com/images/artistbandung/artistbandung2006/artistbandung200600743/153093157-bag-with-money-logo-suck-money-logo.jpg"
+              }
+              width={10}
+            />
+          </Box>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar
+                    size={"sm"}
+                    src={
+                      "https://media-exp1.licdn.com/dms/image/C5603AQG28Bh3km2p_w/profile-displayphoto-shrink_200_200/0/1602101081141?e=1667433600&v=beta&t=fBBQys5CxWYbyeDgzUEjLWO6iiedxXi8nlcJWPfC3LI"
+                    }
+                  />
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={"2xl"}
+                      src={
+                        "https://media-exp1.licdn.com/dms/image/C5603AQG28Bh3km2p_w/profile-displayphoto-shrink_200_200/0/1602101081141?e=1667433600&v=beta&t=fBBQys5CxWYbyeDgzUEjLWO6iiedxXi8nlcJWPfC3LI"
+                      }
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Albert Sebastian </p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+};
